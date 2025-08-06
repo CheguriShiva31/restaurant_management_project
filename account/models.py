532475a2c49ebc804fffdfdbe96adc2eb364a230
CheguriShiva31 <shivacheguri1@gmail.com>
2025-08-06 10:10:52 +0530

    21705

--- account/models.py
+++ account/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class Customer(AbstactUser):
+    phone_number = models.CharField(max_length=15, blank=True)
+    def__str__(self):
+           return self.username
