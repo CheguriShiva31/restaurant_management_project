634875bd14c41f40b60001851fc4e70d73b8ce62
CheguriShiva31 <shivacheguri1@gmail.com>
2025-08-07 10:09:02 +0530

    21709

--- account/views.py
+++ account/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+
 
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import response
+
+def menu_list(request):
+    menu = [{"name": "Margherita Pizza", "description": "Classic pizza with cheese ","price":8.99},{"name":"Veggie Burger", "description":"Burger with grilled vegetables","price":6.49},{"name":"Pasta Alfredo","description":"Creamy Alfredo sauce pasta","price":7.99},]
+    return Response(menu)
+
+    
