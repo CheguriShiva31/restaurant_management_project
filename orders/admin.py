48a659a124a339ca34d471f8ff7fc20cc3f7c48c
CheguriShiva31 <shivacheguri1@gmail.com>
2025-08-07 09:56:41 +0530

    21708

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin
+
 
 # Register your models here.
+from django.contrib import admin
+from.models import Menu, Order
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ['id','name','price']
+    search_fields = ['name']
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id','customer','menu_item','quantity','total_amount','order_status','created_at']
+    list_filter = ['order_status','created_at']
+    search_fields = ['customer__username','menu_item__name']
