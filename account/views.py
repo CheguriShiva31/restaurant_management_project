f6000c7a562133137a3d0887c409652f00a1fca3
CheguriShiva31 <shivacheguri1@gmail.com>
2025-08-07 10:11:46 +0530

    21709

--- account/views.py
+++ account/views.py
@@ -8,4 +8,9 @@ def menu_list(request):
     menu = [{"name": "Margherita Pizza", "description": "Classic pizza with cheese ","price":8.99},{"name":"Veggie Burger", "description":"Burger with grilled vegetables","price":6.49},{"name":"Pasta Alfredo","description":"Creamy Alfredo sauce pasta","price":7.99},]
     return Response(menu)
 
-    
+# Create your urls.py here.
+ 
+from django.urls import path
+from.views import menu_list
+
+urlpatterns = [path('menu/', menu_list, name='menu-list'),]
