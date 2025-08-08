f13018570a7ca45ef1611dff86af72c1f44830fe
CheguriShiva31 <shivacheguri1@gmail.com>
2025-08-08 12:30:33 +0530

    25034

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+
 
 # Create your models here.
+from django.db import models
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length-255)
+    def__str__(self):
+        return self.name
+
+#views.py
+from django.shortcuts import render
+from.models import Restaurant
+
+def home(request):
+    restaurant = Restaurant.objects.first()
+    return render(request,'home.html',{'restaurant_name':restaurant.name if restaurant else'our Restaurant'})
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>{{restaurant_name}}</title>
+    </head>
+    <body>
+        <h1>Welcome to {{restaurant_name}}</h1>
+        </body>
+        </html>
