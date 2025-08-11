7e49e10886a641091fcc938dbf6fcc420dbcd717
CheguriShiva31 <shivacheguri1@gmail.com>
2025-08-11 12:08:25 +0530

    25035

--- orders/models.py
+++ orders/models.py
@@ -4,24 +4,13 @@
 from django.db import models
 
 class Restaurant(models.Model):
-    name = models.CharField(max_length-255)
+    name = models.CharField(max_length-100)
     def__str__(self):
         return self.name
 
 #views.py
 from django.shortcuts import render
 from.models import Restaurant
-
 def home(request):
     restaurant = Restaurant.objects.first()
-    return render(request,'home.html',{'restaurant_name':restaurant.name if restaurant else'our Restaurant'})
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>{{restaurant_name}}</title>
-    </head>
-    <body>
-        <h1>Welcome to {{restaurant_name}}</h1>
-        </body>
-        </html>
+    return render(request,'home.html',{'restaurant_name':restaurant.name if restaurant else 'Our Restaurant'})
