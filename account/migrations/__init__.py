095bba69f0cc5859857ac55395bbfb80dea565cf
CheguriShiva31 <shivacheguri1@gmail.com>
2025-08-13 15:53:26 +0530

    30943

---
+++ account/migrations/__init__.py
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport"
+     content="width=device-width,intial-scale=1.0">
+    <title>contact Us</title>
+    <style>
+        body {
+            font-family: Arial,
+            sans-serif;
+            backgroung-color: #fafafa;
+            color:#333;
+            margin:0;
+            padding:0;
+    
+        }
+        header{
+            background-color: #007BFF;
+            color: white;
+            text-align: center;
+            padding:20px 0;
+        }
+        main {
+            max-width:600px;
+            margin:30 px auto;
+            background: white;
+            border-radius:8px;
+            box-shadow:0 0 10px
+rgba(0,0,0,0,1);
+        }
+        h2{
+            text-align:center;
+            color: #007BFF;
+        }
+        .contact-info {
+            margin-top:20px;
+
+        }
+        .contact-info p{
+            font-size:1.1rem;
+            margin: 10px 0;
+        }
+        footer {
+            background-color: #333;
+            color; white;
+            text-align:center;
+            padding: 10px 0;
+            margin-top: 30px;
+            
+        } 
+        </style>
+ </head>
+ <body>
+     <header>
+     <h1>contact Us</h1>
+     </header>
+
+     <main>
+       <h2>we'd love to hear from You!</h2>
+       <div class ="contact-info"
+           <p><strong> Address:</strong>123 food streetv,hyderbad,india</p>
+           <p><strong> phone:</stong> +91 9787687654 </p>
+           </p><strong> email:</strong> contact@ourrestaurant.com</p>
+           <p><strong>working hours:</strong> mon-sun, 10:00am -10:00pm</p>
+           </div>
+           </main>
+           <footer>
+           <p> 2025 our restaurant.allrights resvered .</p>
+           </footer>
+           </body>
+           </html>
