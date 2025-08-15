91657694b78b8dd26f657481715c76f442b2903a
CheguriShiva31 <shivacheguri1@gmail.com>
2025-08-15 20:28:19 +0530

    30945

--- products/admin.py
+++ products/admin.py
@@ -1,11 +1,12 @@
-from django.contrib import admin
-from .models import *
-
-
-# Custom Admins
-class ItemAdmin(admin.ModelAdmin):
-    list_display = ['item_name','item_price','created_at']
-
-
-# Register your models here.
-admin.site.register(Item,ItemAdmin)
+<!DOCTYPE html>
+<html lang="en">
+     <meta charset="UTF-8">
+     <title>{{restaurant_name}}</title>
+     <style>  body {font-family: Arial, sans-serif; background-color: #faf3e0; text-align:center; padding:50px; color:#333;}
+     h1 {color:#b23a48;} 
+     p {font-size: 18;}
+     </style> </head>
+     <body>
+     <h1> {{restaurant_name}}</h1> <p> Welcome to  {{restaurant_name}} ! We're delighted to have you here.</p>
+     </body>
+     </html>
