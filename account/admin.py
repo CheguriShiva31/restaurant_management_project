960e726a5f2bd6dfa6b8f5ddbeb680930287464d
CheguriShiva31 <shivacheguri1@gmail.com>
2025-08-15 20:44:42 +0530

    30941

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,4 @@
-from django.contrib import admin
-
-# Register your models here.
+from django.shortcuts import render 
+def menu_list(request):
+    menu_items = [{"name": "Margherita pizza", "price": 8.99}, {"name": "pasta Alfredo","price:10.99"}, {"name":"chocolate lava cake","price":4.99},]
+    return render(request,"menu_list.html",{"menu_items": menu_itemms})
