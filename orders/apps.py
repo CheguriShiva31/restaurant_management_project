909564d01a66e60009d488f579e143bd4410f0e8
CheguriShiva31 <shivacheguri1@gmail.com>
2025-08-19 15:39:54 +0530

    34900

--- orders/apps.py
+++ orders/apps.py
@@ -1,6 +1,10 @@
-from django.apps import AppConfig
-
-
-class OrdersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'orders'
+from django.http import JsonResponse
+from.models import MenuItem
+def menu_item_view(request):
+    try:
+        list(MenuItem.objects.values("id","name","price"))
+        return JsonResponse({"menu_items": items},status=200)
+        except MenuItem.DoesNotExists:
+            return JsonResponse({"error:"No menu items found"}, status=404)
+            except Exception as e:
+                return JsonResponse({"error":f"something went wrong: {str(e)}"}, status=500)
